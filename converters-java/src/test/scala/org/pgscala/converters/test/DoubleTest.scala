f98dc97b47b271106a4db8fe4d0c849310402a30
Ana Marjanica <ana@element.hr>
2012-03-14 11:10:55 +0100

    fixed other tests

--- converters-java/src/test/scala/org/pgscala/converters/test/DoubleTest.scala
+++ converters-java/src/test/scala/org/pgscala/converters/test/DoubleTest.scala
@@ -12,77 +12,77 @@ class DoubleTest extends FeatureSpec with GivenWhenThen with ShouldMatchers{
     info("I want to test if PGNullableDoubleConverter works correctly, both in 2 way conversion")
     info("I am going to perform tests for the Double boundary cases")
 
-//    scenario("Double to String Nr. 1"){
-//      given ("a starting Double value of positive infinity")
-//      val d = Double.PositiveInfinity
-//      when ("that value is converted to String")
-//      val res = PGNullableDoubleConverter doubleToString d
-//      then ("""it should return a String value "%s"""" format d.toString)
-//      res should equal (d.toString)
-//    }
-//
-//    scenario("Double to String Nr. 2"){
-//      given ("a starting Double value of negative infinity")
-//      val d = Double.NegativeInfinity
-//      when ("that value is converted to String")
-//      val res = PGNullableDoubleConverter doubleToString d
-//      then ("""it should return a String value "%s"""" format d.toString)
-//      res should equal (d.toString)
-//    }
-//
-//    scenario("Double to String Nr. 3"){
-//      val d = 0d
-//      given ("a starting Double value of %s" format d toString)
-//      when ("that value is converted to String")
-//      val res = PGNullableDoubleConverter doubleToString d
-//      then ("""it should return a String value "%s"""" format d.toString)
-//      res should equal (d.toString)
-//    }
-//
-//    scenario("Double to String Nr. 4"){
-//      val d = Double.NaN
-//      given ("a starting value of NaN")
-//      when ("that value is converted to String")
-//      val res = PGNullableDoubleConverter doubleToString d
-//      then ("""it should return a String value "%s"""" format d.toString)
-//      res should equal (d.toString)
-//    }
-//
-//    scenario("String to Double Nr. 1"){
-//      given ("""a starting String value of "Infinity"""")
-//      val s =  Double.PositiveInfinity.toString
-//      when ("that value is converted to Double")
-//      val res = PGNullableDoubleConverter stringToDouble s
-//      then ("it should return a Double value of %s" format s)
-//      res.toString should equal (s)
-//    }
-//
-//    scenario("String to Double Nr. 2"){
-//      given ("""a starting String value of "-Infinity"""")
-//      val s =  Double.NegativeInfinity.toString
-//      when ("that value is converted to Double")
-//      val res = PGNullableDoubleConverter stringToDouble s
-//      then ("it should return a Double value of %s" format s)
-//      res.toString should equal (s)
-//    }
-//
-//    scenario("String to Double Nr. 3"){
-//      given ("""a starting String value of "NaN"""")
-//      val s =  "NaN"
-//      when ("that value is converted to Double")
-//      val res = PGNullableDoubleConverter stringToDouble s
-//      then ("it should return a Double value of %s" format s)
-//      res.isNaN() should equal (Double.NaN.isNaN())
-//    }
-//
-//    scenario("String to Double Nr. 4"){
-//      given ("""a starting String value of "0"""")
-//      val s =  "0"
-//      when ("that value is converted to Double")
-//      val res = PGNullableDoubleConverter stringToDouble s
-//      then ("it should return a Double value of %s" format s)
-//      res should equal (0d)
-//    }
+    scenario("Double to String Nr. 1"){
+      given ("a starting Double value of positive infinity")
+      val d = Double.PositiveInfinity
+      when ("that value is converted to String")
+      val res = PGNullableDoubleConverter doubleToString d
+      then ("""it should return a String value "%s"""" format d.toString)
+      res should equal (d.toString)
+    }
+
+    scenario("Double to String Nr. 2"){
+      given ("a starting Double value of negative infinity")
+      val d = Double.NegativeInfinity
+      when ("that value is converted to String")
+      val res = PGNullableDoubleConverter doubleToString d
+      then ("""it should return a String value "%s"""" format d.toString)
+      res should equal (d.toString)
+    }
+
+    scenario("Double to String Nr. 3"){
+      val d = 0d
+      given ("a starting Double value of %s" format d toString)
+      when ("that value is converted to String")
+      val res = PGNullableDoubleConverter doubleToString d
+      then ("""it should return a String value "%s"""" format d.toString)
+      res should equal (d.toString)
+    }
+
+    scenario("Double to String Nr. 4"){
+      val d = Double.NaN
+      given ("a starting value of NaN")
+      when ("that value is converted to String")
+      val res = PGNullableDoubleConverter doubleToString d
+      then ("""it should return a String value "%s"""" format d.toString)
+      res should equal (d.toString)
+    }
+
+    scenario("String to Double Nr. 1"){
+      given ("""a starting String value of "Infinity"""")
+      val s =  Double.PositiveInfinity.toString
+      when ("that value is converted to Double")
+      val res = PGNullableDoubleConverter stringToDouble s
+      then ("it should return a Double value of %s" format s)
+      res.toString should equal (s)
+    }
+
+    scenario("String to Double Nr. 2"){
+      given ("""a starting String value of "-Infinity"""")
+      val s =  Double.NegativeInfinity.toString
+      when ("that value is converted to Double")
+      val res = PGNullableDoubleConverter stringToDouble s
+      then ("it should return a Double value of %s" format s)
+      res.toString should equal (s)
+    }
+
+    scenario("String to Double Nr. 3"){
+      given ("""a starting String value of "NaN"""")
+      val s =  "NaN"
+      when ("that value is converted to Double")
+      val res = PGNullableDoubleConverter stringToDouble s
+      then ("it should return a Double value of %s" format s)
+      res.isNaN() should equal (Double.NaN.isNaN())
+    }
+
+    scenario("String to Double Nr. 4"){
+      given ("""a starting String value of "0"""")
+      val s =  "0"
+      when ("that value is converted to Double")
+      val res = PGNullableDoubleConverter stringToDouble s
+      then ("it should return a Double value of %s" format s)
+      res should equal (0d)
+    }
 
     scenario("POSTGRESQL: Double to String Nr. 1"){
       TestDb.using{ db =>
@@ -168,7 +168,7 @@ class DoubleTest extends FeatureSpec with GivenWhenThen with ShouldMatchers{
         when("that value is converted to Double")
         val res = d.toDouble
         then ("It should return a Double value %s" format res)
-        res should equal(Double.NaN)
+        res.isNaN should equal(Double.NaN.isNaN)
       }
     }
 
--- converters-java/src/test/scala/org/pgscala/converters/test/FloatTest.scala
+++ converters-java/src/test/scala/org/pgscala/converters/test/FloatTest.scala
@@ -12,100 +12,100 @@ class FloatTest  extends FeatureSpec with GivenWhenThen with ShouldMatchers {
     info("I want to test if PGNullableFloatConverter works correctly, both in 2 way conversion")
     info("I am going to perform tests for the Float special cases")
 
-//    scenario("Float to String Nr. 1"){
-//      //select 3.4028235E38::float8;
-//      val f = Float.MaxValue
-//      given ("a starting Float value of %s" format f)
-//      when ("that value is converted to String")
-//      val res = PGNullableFloatConverter floatToString f
-//      then ("""It should return a String value "%s"""" format res)
-//      res should equal (f.toString)
-//    }
-//
-//    scenario("Float to String Nr. 2"){
-//      //select -3.4028235E38::float8;
-//      val f = Float.MinValue
-//      given ("a starting Float value of %s" format f)
-//      when ("that value is converted to String")
-//      val res = PGNullableFloatConverter floatToString f
-//      then ("""It should return a String value "%s"""" format res)
-//      res should equal (f.toString)
-//    }
-//
-//    scenario("Float to String Nr. 3"){
-//      //select 'NaN'::float8;
-//      val f = Float.NaN
-//      given ("a starting value of %s" format f)
-//      when ("that value is converted to String")
-//      val res = PGNullableFloatConverter floatToString f
-//      then ("""It should return a String value "%s"""" format res)
-//      res should equal (f.toString)
-//    }
-//
-//    scenario("Float to String Nr. 4"){
-//      //select 'Infinity'::float8;
-//      val f = Float.PositiveInfinity
-//      given ("a starting value of %s" format f)
-//      when ("that value is converted to String")
-//      val res = PGNullableFloatConverter floatToString f
-//      then ("""It should return a String value "%s"""" format res)
-//      res should equal (f.toString)
-//    }
-//
-//    scenario("Float to String Nr. 5"){
-//      //select '-Infinity'::float8;
-//      val f = Float.NegativeInfinity
-//      given ("a starting value of %s" format f)
-//      when ("that value is converted to String")
-//      val res = PGNullableFloatConverter floatToString f
-//      then ("""It should return a String value "%s"""" format res)
-//      res should equal (f.toString)
-//    }
-//
-//    scenario("String to Float Nr. 1"){
-//      val s = "3.4028235E38"
-//      given ("""a starting String value of "%s""" format s)
-//      when ("that value is converted to Float")
-//      val res = PGNullableFloatConverter stringToFloat s
-//      then ("It should return a Float value %s" format res)
-//      res.toString should equal (s)
-//    }
-//
-//    scenario("String to Float Nr. 2"){
-//      val s = "-3.4028235E38"
-//      given ("""a starting String value of "%s""" format s)
-//      when ("that value is converted to Float")
-//      val res = PGNullableFloatConverter stringToFloat s
-//      then ("It should return a Float value %s" format res)
-//      res.toString should equal (s)
-//    }
-//
-//    scenario("String to Float Nr. 3"){
-//      val s = "NaN"
-//      given ("""a starting String value of "%s""" format s)
-//      when ("that value is converted to Float")
-//      val res = PGNullableFloatConverter stringToFloat s
-//      then ("It should return a Float value %s" format res)
-//      res.isNaN should equal (Float.NaN.isNaN)
-//    }
-//
-//    scenario("String to Float Nr. 4"){
-//      val s = "Infinity"
-//      given ("""a starting String value of "%s""" format s)
-//      when ("that value is converted to Float")
-//      val res = PGNullableFloatConverter stringToFloat s
-//      then ("It should return a Float value %s" format res)
-//      res.toString should equal (s)
-//    }
-//
-//    scenario("String to Float Nr. 5"){
-//      val s = "-Infinity"
-//      given ("""a starting String value of "%s""" format s)
-//      when ("that value is converted to Float")
-//      val res = PGNullableFloatConverter stringToFloat s
-//      then ("It should return a Float value %s" format res)
-//      res.toString should equal (s)
-//    }
+    scenario("Float to String Nr. 1"){
+      //select 3.4028235E38::float8;
+      val f = Float.MaxValue
+      given ("a starting Float value of %s" format f)
+      when ("that value is converted to String")
+      val res = PGNullableFloatConverter floatToString f
+      then ("""It should return a String value "%s"""" format res)
+      res should equal (f.toString)
+    }
+
+    scenario("Float to String Nr. 2"){
+      //select -3.4028235E38::float8;
+      val f = Float.MinValue
+      given ("a starting Float value of %s" format f)
+      when ("that value is converted to String")
+      val res = PGNullableFloatConverter floatToString f
+      then ("""It should return a String value "%s"""" format res)
+      res should equal (f.toString)
+    }
+
+    scenario("Float to String Nr. 3"){
+      //select 'NaN'::float8;
+      val f = Float.NaN
+      given ("a starting value of %s" format f)
+      when ("that value is converted to String")
+      val res = PGNullableFloatConverter floatToString f
+      then ("""It should return a String value "%s"""" format res)
+      res should equal (f.toString)
+    }
+
+    scenario("Float to String Nr. 4"){
+      //select 'Infinity'::float8;
+      val f = Float.PositiveInfinity
+      given ("a starting value of %s" format f)
+      when ("that value is converted to String")
+      val res = PGNullableFloatConverter floatToString f
+      then ("""It should return a String value "%s"""" format res)
+      res should equal (f.toString)
+    }
+
+    scenario("Float to String Nr. 5"){
+      //select '-Infinity'::float8;
+      val f = Float.NegativeInfinity
+      given ("a starting value of %s" format f)
+      when ("that value is converted to String")
+      val res = PGNullableFloatConverter floatToString f
+      then ("""It should return a String value "%s"""" format res)
+      res should equal (f.toString)
+    }
+
+    scenario("String to Float Nr. 1"){
+      val s = "3.4028235E38"
+      given ("""a starting String value of "%s""" format s)
+      when ("that value is converted to Float")
+      val res = PGNullableFloatConverter stringToFloat s
+      then ("It should return a Float value %s" format res)
+      res.toString should equal (s)
+    }
+
+    scenario("String to Float Nr. 2"){
+      val s = "-3.4028235E38"
+      given ("""a starting String value of "%s""" format s)
+      when ("that value is converted to Float")
+      val res = PGNullableFloatConverter stringToFloat s
+      then ("It should return a Float value %s" format res)
+      res.toString should equal (s)
+    }
+
+    scenario("String to Float Nr. 3"){
+      val s = "NaN"
+      given ("""a starting String value of "%s""" format s)
+      when ("that value is converted to Float")
+      val res = PGNullableFloatConverter stringToFloat s
+      then ("It should return a Float value %s" format res)
+      res.isNaN should equal (Float.NaN.isNaN)
+    }
+
+    scenario("String to Float Nr. 4"){
+      val s = "Infinity"
+      given ("""a starting String value of "%s""" format s)
+      when ("that value is converted to Float")
+      val res = PGNullableFloatConverter stringToFloat s
+      then ("It should return a Float value %s" format res)
+      res.toString should equal (s)
+    }
+
+    scenario("String to Float Nr. 5"){
+      val s = "-Infinity"
+      given ("""a starting String value of "%s""" format s)
+      when ("that value is converted to Float")
+      val res = PGNullableFloatConverter stringToFloat s
+      then ("It should return a Float value %s" format res)
+      res.toString should equal (s)
+    }
 
     /*
      * POSTGRES
@@ -168,34 +168,34 @@ class FloatTest  extends FeatureSpec with GivenWhenThen with ShouldMatchers {
 
     scenario("POSTGRESQL: String to Float Nr. 1"){
       TestDb.using{ db =>
-        val f = db.row("select 3.4028235E38::float8;"){rS => rS.get[String](1)}.get
+        val f = db.row("select 3.4028235E38::numeric;"){rS => rS.get[String](1)}.get
         given(" a starting String value for %s" format f)
         when("that value is converted to Float")
         val res = f.toFloat
         then ("It should return a Float value %s" format res)
-        res should equal(3.4028235E38)
+        res should equal(3.4028235E38.toFloat)
       }
     }
 
     scenario("POSTGRESQL: String to Float Nr. 2"){
       TestDb.using{ db =>
-        val f = db.row("select -3.4028235E38::float8;"){rS => rS.get[String](1)}.get
+        val f = db.row("select -3.4028235E38::numeric;"){rS => rS.get[String](1)}.get
         given(" a starting String value for %s" format f)
         when("that value is converted to Float")
         val res = f.toFloat
         then ("It should return a Float value %s" format res)
-        res should equal(-3.4028235E38)
+        res should equal(-3.4028235E38.toFloat)
       }
     }
 
     scenario("POSTGRESQL: String to Float Nr. 3"){
       TestDb.using{ db =>
-        val f = db.row("select 'NaN'::float8;"){rS => rS.get[String](1)}.get
+        val f = db.row("select 'NaN'::numeric;"){rS => rS.get[String](1)}.get
         given(" a starting String value for %s" format f)
         when("that value is converted to Float")
         val res = f.toFloat
         then ("It should return a Float value %s" format res)
-        res should equal(Float.NaN)
+        res.isNaN should equal(Float.NaN.isNaN)
       }
     }
 
